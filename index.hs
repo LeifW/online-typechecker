37f6f37f5fdaffff4420e897d12aa21d6161bd2f
Leif Warner <abimelech@gmail.com>
2013-12-09 22:38:43 -0800

    Upgrade to WAI / Warp 2.0.0

--- index.hs
+++ index.hs
@@ -4,9 +4,9 @@ import Network.HTTP.Types
 import Network.Wai.Application.Static (staticApp, defaultWebAppSettings)
 import Network.Wai.Handler.Warp (run)
 import Data.Conduit (($$))
-import Data.Conduit.Filesystem (sinkFile)
-import Control.Monad.Trans (liftIO)
-import Filesystem.Path.CurrentOS (fromText, (</>))
+import Data.Conduit.Binary (sinkHandle)
+import System.IO (withFile, IOMode(WriteMode))
+import Control.Applicative ((<$>))
 import Data.Text (unpack)
 import Data.ByteString.Lazy.Char8 (pack)
 import IdrisClient (runClient)
@@ -16,18 +16,16 @@ static = staticApp $ defaultWebAppSettings "."
 
 application :: Application
 application req = case ((parseMethod . requestMethod) req, pathInfo req) of
-  --(Right GET, []) -> return $ ResponseFile status200 [] "index.html" Nothing
   (Right GET, []) -> static req { pathInfo = ["index.html"] }
   (Right POST, ["sources", name]) -> do
-    requestBody req $$ sinkFile ("sources" </> fromText name)
-    report <- liftIO $ runClient (":l " ++ unpack name)
-    return $ responseLBS status200 [("Content-Type", "text/plain")] (pack report)
-    --return $ ResponseFile status200 [] "index.html" Nothing
+    withFile ("sources/" ++ unpack name) WriteMode $ \h->
+      requestBody req $$ sinkHandle h
+    let report = runClient (":l " ++ unpack name)
+    responseLBS
+      status200
+      [("Content-Type", "text/plain")] .
+      pack <$> report
   _ -> static req
-  --liftIO $ print $ pathInfo req
-  
-  --
-  --return $ responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
 
 main :: IO ()
 main = run 3000 application
